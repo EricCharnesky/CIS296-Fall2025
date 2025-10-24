637ab97704c1924ba3df973d6ad4daf2f3f52188
Eric Charnesky <2469199+EricCharnesky@users.noreply.github.com>
2025-10-23 20:27:19 -0400

    added getStudent

--- Week9-MoreJava/src/main/java/org/example/CollegeDB.java
+++ Week9-MoreJava/src/main/java/org/example/CollegeDB.java
@@ -10,6 +10,59 @@ public class CollegeDB {
         return connection;
     }
 
+    public static void updateStudent(Student student){
+        try {
+            Connection connection = getConnection();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "update student set FirstName = ?, LastName = ?, Major = ?, GPA = ? where ID = ?");
+
+            // first value is index 1, it's ok to cry
+            preparedStatement.setString(1, student.getFirstName());
+            preparedStatement.setString(2, student.getLastName());
+            preparedStatement.setString(3, student.getMajor());
+            preparedStatement.setDouble(4, student.getGpa());
+            preparedStatement.setInt(5, student.getID());
+
+            preparedStatement.execute();
+
+            connection.close();
+        } catch ( SQLException e){
+            System.out.println("SQLException: " + e.getMessage());
+        }
+    }
+
+    public static ArrayList<Student> getStudent(int id){
+        ArrayList<Student> students = new ArrayList<Student>();
+
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM Student where id = ?");
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet studentsQuery = preparedStatement.executeQuery();
+
+            while (studentsQuery.next()) {
+                String firstName = studentsQuery.getString("FirstName");
+                String lastName = studentsQuery.getString("LastName");
+                String major = studentsQuery.getString("Major");
+                double gpa = studentsQuery.getDouble("GPA");
+
+                students.add(new Student(id, firstName, lastName, major, gpa));
+            }
+            studentsQuery.close();
+
+            connection.close();
+        } catch ( SQLException e){
+            System.out.println("SQLException: " + e.getMessage());
+        }
+
+        return students;
+    }
+
     public static ArrayList<Student> getStudents(){
         ArrayList<Student> students = new ArrayList<Student>();
 
--- Week9-MoreJava/src/main/java/org/example/Main.java
+++ Week9-MoreJava/src/main/java/org/example/Main.java
@@ -14,40 +14,60 @@ import java.time.*;
 
 public class Main {
 
-
-
+    public static Student getStudentInfo(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter firstname, lastname, major, gpa");
+        String text = keyboard.nextLine();
+        String[] split = text.split(",");
+        String firstName = split[0].trim();
+        String lastName = split[1].trim();
+        String major = split[2].trim();
+        double gpa = Double.parseDouble(split[3]);
+        return new Student(0, firstName,lastName,major,gpa);
+    }
 
     public static void main(String[] args) {
 
         Scanner keyboard = new Scanner(System.in);
         int choice = 0;
 
-        while ( choice != 3 ) {
+        while ( choice != 4 ) {
 
-            System.out.println("Enter 1 to add, 2 to read, 3 to quit");
+            System.out.println("Enter 1 to add, 2 to read, 3 to update, 4 to quit");
             choice = Integer.parseInt(keyboard.nextLine());
 
             if (choice == 1) {
-
-                    System.out.println("Enter firstname, lastname, major, gpa");
-                    String text = keyboard.nextLine();
-                    String[] split = text.split(",");
-                    String firstName = split[0];
-                    String lastName = split[1];
-                    String major = split[2];
-                    double gpa = Double.parseDouble(split[3]);
-
-                CollegeDB.addStudent(new Student(0,  firstName, lastName, major, gpa));
-
-
+                CollegeDB.addStudent(getStudentInfo());
             } else if (choice == 2) {
-
                 ArrayList<Student> students = CollegeDB.getStudents();
 
                 for (Student student : students) {
                     System.out.println(student);
                 }
 
+            }
+            else if (choice == 3) {
+
+                System.out.println("Enter ID of student to update");
+                int id =  Integer.parseInt(keyboard.nextLine());
+
+                ArrayList<Student> students = CollegeDB.getStudent(id);
+
+                boolean foundStudent = false;
+                // TODO - fix later to find just 1
+                for ( Student student : students) {
+                    if ( student.getID() == id ) {
+                        System.out.println(student);
+                        Student studentInfo = getStudentInfo();
+                        studentInfo.setID(id);
+                        CollegeDB.updateStudent(studentInfo);
+                        foundStudent = true;
+                        break;
+                    }
+                }
+                if ( !foundStudent ) {
+                    System.out.println("ID not found!");
+                }
             }
         }
 
